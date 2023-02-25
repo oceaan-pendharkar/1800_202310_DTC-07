a86eaf972c2c2aef47904d15a84aa414ac12084e
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-02-25 13:52:07 -0800

    Add contact user link

--- scripts/message.js
+++ scripts/message.js
@@ -3,24 +3,28 @@ function submitMessage() {
     // just a placeholder until we know what to do here
     let username = document.getElementById("username").value;
     let message = document.getElementById("message").value;
-
-    $("#message-board").append(`<div class="message container"><div class="row">
-                <div class="col-2">
+    $("#message-board").append(`
+    <div class="message container">
+        <div class="row">
+            <div class="col-2">
                     <img width="80px" height="80px" src="./images/userplaceholder.jpeg">
-                </div>
-                <div class="col-10">
-                    <div class="row">
-                        <div class="col-12">
-                            <h5>${username}</h5>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <p>${message}</p>
-                        </div>
+            </div>
+            <div class="col-10">
+                 <div class="row">
+                    <div class="col-12">
+                        <h5>${username}</h5>
                     </div>
                 </div>
-            </div></div>`);
+                <div class="row">
+                    <div class="col-12">
+                        <p>${message}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <a class="link">Contact User</a>
+    </div>`
+    );
     console.log(username, message);
 }
 
