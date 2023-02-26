54f7404f4a5ffc87534c0139b5442a7462e04d8d
I-wen Su <grace2268@hotmail.com>
2023-02-26 03:28:30 -0800

    size down user profile img

--- scripts/message.js
+++ scripts/message.js
@@ -3,15 +3,15 @@ function submitMessage() {
     // just a placeholder until we know what to do here
     let username = document.getElementById("username").value;
     let message = document.getElementById("message").value;
-    $("#message-board").append(`
+    $("#message-board").prepend(`
     <div class="message container">
         <div class="row">
-            <div class="col-lg-2 row-md">
+            <div class="col-2">
                     <img width="100%" height="auto" src="./images/userplaceholder.jpeg">
             </div>
-            <div class="col-lg-10 row-md">
+            <div class="col-10">
                  <div class="row">
-                    <div class="col-12 mt-3">
+                    <div class="col-12">
                         <h5>${username}</h5>
                     </div>
                 </div>
