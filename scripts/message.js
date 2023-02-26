1397eec1c4d694ecb8c184ea5eb427a3e8913781
I-wen Su <grace2268@hotmail.com>
2023-02-25 23:33:09 -0800

    nav, footer nav and message board layout

--- scripts/message.js
+++ scripts/message.js
@@ -6,23 +6,24 @@ function submitMessage() {
     $("#message-board").append(`
     <div class="message container">
         <div class="row">
-            <div class="col-2">
-                    <img width="80px" height="80px" src="./images/userplaceholder.jpeg">
+            <div class="col-lg-2 row-md">
+                    <img width="100%" height="auto" src="./images/userplaceholder.jpeg">
             </div>
-            <div class="col-10">
+            <div class="col-lg-10 row-md">
                  <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 mt-3">
                         <h5>${username}</h5>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-12">
                         <p>${message}</p>
+                        <a class="link">Contact User</a>
                     </div>
                 </div>
             </div>
         </div>
-            <a class="link">Contact User</a>
+            
     </div>`
     );
     console.log(username, message);
