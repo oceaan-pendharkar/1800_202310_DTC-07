298f992ae3c6cea0ce320ef8b5ecdb3004b3691e
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-02-28 15:55:27 -0800

    edit for double "good"

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,33 @@
 function getNameFromAuth() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
-        if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
-            user_Name = user.displayName;
+  firebase.auth().onAuthStateChanged(user => {
+    // Check if a user is signed in:
+    if (user) {
+      // Do something for the currently logged-in user here: 
+      console.log(user.uid); //print the uid in the browser console
+      console.log(user.displayName);  //print the user name in the browser console
+      user_Name = user.displayName;
 
-            //method #1:  insert with JS
-            //document.getElementById("name-goes-here").innerText = userName;    
-            // method #2:  insert using jquery
-            $("#name-goes-here").text(userName); //using jquery
-            //method #3:  insert using querySelector
-            //document.querySelector("#name-goes-here").innerText = userName
+      //method #1:  insert with JS
+      //document.getElementById("name-goes-here").innerText = userName;    
+      // method #2:  insert using jquery
+      $("#name-goes-here").text(userName); //using jquery
+      //method #3:  insert using querySelector
+      //document.querySelector("#name-goes-here").innerText = userName
 
-        } else {
-            // No user is signed in.
-        }
-    });
+    } else {
+      // No user is signed in.
+    }
+  });
 }
 getNameFromAuth(); //run the function
 
 const time = new Date().getHours();
 let greeting;
 if (time < 12) {
-  greeting = "Good morning!";
+  greeting = "morning!";
 } else if (time < 18) {
-  greeting = "Good afternoon@";
+  greeting = "afternoon!";
 } else {
-  greeting = "Good evening!";
+  greeting = "evening!";
 }
 document.getElementById("time-of-day").innerHTML = greeting;
