cd43dbfee8a9e75ccb555e038b701607627b53f7
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-03-01 11:15:06 -0800

    fix close buttons bug

--- scripts/ihave.js
+++ scripts/ihave.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-    $(".button-other").click(function(){
-      alert(Hello);
-    });
+$(document).ready(function () {
+  $(".button-other").click(function () {
+    alert(Hello);
+  });
+});
+
+$(".close-btn").click(function () {
+  $("#edit-items").modal('hide');
 });
