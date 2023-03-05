4af107f7ab74fc9b1406508cda978653d741eb71
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-03-05 14:41:26 -0800

    Edit profile to populate with database info

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,20 +1,28 @@
-// function insertNameFromFirestore() {
-//     // to check if the user is logged in:
-//     firebase.auth().onAuthStateChanged(user => {
-//         if (user) {
-//             console.log(user.uid); // let me to know who is the user that logged in to get the UID
-//             currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
-//             currentUser.get().then(userDoc => {
-//                 //get the user name
-//                 var userName = userDoc.data().name;
-//                 console.log(userName);
-//                 //$("#name-goes-here").text(userName); //jquery
-//                 document.getElementById("name-goes-here").innerText = userName;
-//             })
-//         }
-//     })
-// }
-// insertNameFromFirestore()
+function insertProfileInfo() {
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // let me to know who is the user that logged in to get the UID
+            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+            currentUser.get().then(userDoc => {
+                //get the user name
+                var userName = userDoc.data().name;
+                var email = userDoc.data().email;
+                var phone = userDoc.data().phone;
+                var neighbourhood = userDoc.data().neighbourhood;
+                var items = userDoc.data().items;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); //jquery
+                document.getElementById("name-goes-here").innerText = userName;
+                document.getElementById("email-goes-here").innerText = email;
+                document.getElementById("phone-goes-here").innerText = phone;
+                document.getElementById("neighbourhood-goes-here").innerText = neighbourhood;
+                document.getElementById("items-go-here").innerText = items;
+            })
+        }
+    })
+}
+insertProfileInfo()
 
 
 
