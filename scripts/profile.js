45780955df18af03077dd95e42ac1339ff1e6c97
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-03-06 08:13:08 -0800

    edited with bug, need to press refresh to populate

--- scripts/profile.js
+++ scripts/profile.js
@@ -33,12 +33,17 @@ function editProfile() {
             console.log(user.uid); // let me to know who is the user that logged in to get the UID
             currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
             currentUser.get().then(userDoc => {
-                //get the user name
-                var username = document.getElementById("username").value;
-                var photo = document.getElementById("profile-image").value;
-                var email = document.getElementById("email").value;
-                var phone = document.getElementById("phone").value;
-                var neighbourhood = document.getElementById("neighbourhood").value;
+                var username;
+                var photo;
+                var email;
+                var phone;
+                var neighbourhood;
+
+                username = document.getElementById("username").value;
+                photo = document.getElementById("profile-image").value;
+                email = document.getElementById("email").value;
+                phone = document.getElementById("phone").value;
+                neighbourhood = document.getElementById("neighbourhood").value;
 
                 //change photo
                 document.getElementById("photo-goes-here").src = photo;
@@ -67,13 +72,14 @@ $("#edit-profile").click(function () {
 
 $("#submit-changes").click(function () {
     editProfile();
-    $("#profile").show();
     $("#profile-editing").hide();
+    $("#profile").show();
 });
 
 const setup = () => {
     console.log("Hello World!")
     $("#profile-editing").hide();
+
 }
 
 $(document).ready(setup)
