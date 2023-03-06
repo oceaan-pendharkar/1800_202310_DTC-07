409c35467d26f2bc41a04908ee7bade054994563
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-03-06 08:35:47 -0800

    Fix empty string bug

--- scripts/profile.js
+++ scripts/profile.js
@@ -33,17 +33,29 @@ function editProfile() {
             console.log(user.uid); // let me to know who is the user that logged in to get the UID
             currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
             currentUser.get().then(userDoc => {
-                var username;
-                var photo;
-                var email;
-                var phone;
-                var neighbourhood;
+                var username = document.getElementById("username").value;
+                var photo = document.getElementById("profile-image").value;
+                var email = document.getElementById("email").value;
+                var phone = document.getElementById("phone").value;
+                var neighbourhood = document.getElementById("neighbourhood").value;
 
-                username = document.getElementById("username").value;
-                photo = document.getElementById("profile-image").value;
-                email = document.getElementById("email").value;
-                phone = document.getElementById("phone").value;
-                neighbourhood = document.getElementById("neighbourhood").value;
+                //make sure user entered something new in each field if we are going to change it
+                if (username == "") {
+                    username = userDoc.data().name;
+                }
+                if (photo == "") {
+                    photo = userDoc.data().photo;
+                }
+                if (email == "") {
+                    email = userDoc.data().email;
+                }
+                if (phone == "") {
+                    phone = userDoc.data().phone;
+                }
+
+                if (neighbourhood == "") {
+                    neighbourhood = userDoc.data().neighbourhood;
+                }
 
                 //change photo
                 document.getElementById("photo-goes-here").src = photo;
