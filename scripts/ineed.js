c9a0583ffd858287540e883fa3add99878e9bca6
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-03-07 17:08:40 -0800

    try to link dropdown menu to firestore list

---
+++ scripts/ineed.js
@@ -0,0 +1,25 @@
+function populateDropdown() {
+    // Fetch the documents from the collection
+    resources.get().then((querySnapshot) => {
+      // Loop through each document and get its string fields
+      const values = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        for (const key in data) {
+          if (typeof data[key] === "string") {
+            values.push(data[key]);
+          }
+        }
+      });
+  
+      // Populate the dropdown with the values
+      values.forEach((value) => {
+        const option = document.createElement("option");
+        option.text = value;
+        resource-dropdown.add(option);
+      });
+    });
+  }
+  
+  // Call the populateDropdown function to start populating the dropdown
+  populateDropdown();
