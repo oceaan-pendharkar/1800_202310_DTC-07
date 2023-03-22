33368f58da48c2fa3ef6c848a334c45e38ae0fc5
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-03-22 14:16:54 -0700

    Hoda office hours updates

--- scripts/profile.js
+++ scripts/profile.js
@@ -45,63 +45,63 @@ function editUserInfo() {
     document.getElementById('personalInfoFields').disabled = false;
 }
 
-function saveUserInfo() {
+// function saveUserInfo() {
 
-    //get the values from the form fields
-    var name = document.getElementById("nameInput").value;
-    var neighbourhood = document.getElementById("neighbourhoodInput").value;
-    var city = document.getElementById("cityInput").value;
-    var phone = document.getElementById("phoneInput").value;
+//     //get the values from the form fields
+//     var name = document.getElementById("nameInput").value;
+//     var neighbourhood = document.getElementById("neighbourhoodInput").value;
+//     var city = document.getElementById("cityInput").value;
+//     var phone = document.getElementById("phoneInput").value;
 
-    //update the user document with the new data
-    currentUser.update({
-        name: name,
-        neighbourhood: neighbourhood,
-        city: city,
-        phone: phone
-    })
+//     //update the user document with the new data
+//     currentUser.update({
+//         name: name,
+//         neighbourhood: neighbourhood,
+//         city: city,
+//         phone: phone
+//     })
 
-    //Disable the form fields
-    document.getElementById('personalInfoFields').disabled = true;
-}
+//     //Disable the form fields
+//     document.getElementById('personalInfoFields').disabled = true;
+// }
 
 // THIS IS THE FUNCTION FOR PROFILE PIC SAVING BUT IT DOESN'T WORK YET
-// function saveUserInfo() {
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         var storageRef = storage.ref("images/" + user.uid + ".jpg");
+function saveUserInfo() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        var storageRef = storage.ref("images/" + user.uid + ".jpg");
 
-//         //Asynch call to put File Object (global variable ImageFile) onto Cloud
-//         storageRef.put(ImageFile)
-//             .then(function () {
-//                 console.log('Uploaded to Cloud Storage.');
+        //Asynch call to put File Object (global variable ImageFile) onto Cloud
+        storageRef.put(ImageFile)
+            .then(function () {
+                console.log('Uploaded to Cloud Storage.');
 
-//                 //Asynch call to get URL from Cloud
-//                 storageRef.getDownloadURL()
-//                     .then(function (url) { // Get "url" of the uploaded file
-//                         console.log("Got the download URL.");
-//                         //get values from the from
-//                         userName = document.getElementById('nameInput').value;
-//                         userNeighbourhood = document.getElementById('neighbourhoodInput').value;
-//                         userCity = document.getElementById('cityInput').value;
-//                         userPhone = document.getElementById('phoneInput').value;
+                //Asynch call to get URL from Cloud
+                storageRef.getDownloadURL()
+                    .then(function (url) { // Get "url" of the uploaded file
+                        console.log("Got the download URL.");
+                        //get values from the from
+                        userName = document.getElementById('nameInput').value;
+                        userNeighbourhood = document.getElementById('neighbourhoodInput').value;
+                        userCity = document.getElementById('cityInput').value;
+                        userPhone = document.getElementById('phoneInput').value;
 
-//                         //Asynch call to save the form fields into Firestore.
-//                         db.collection("users").doc(user.uid).update({
-//                             name: userName,
-//                             neighbourhood: userNeighbourhood,
-//                             city: userCity,
-//                             phone: userPhone,
-//                             profilePic: url // Save the URL into users collection
-//                         })
-//                             .then(function () {
-//                                 console.log('Added Profile Pic URL to Firestore.');
-//                                 console.log('Saved use profile info');
-//                                 document.getElementById('personalInfoFields').disabled = true;
-//                             })
-//                     })
-//             })
-//     })
-// }
+                        //Asynch call to save the form fields into Firestore.
+                        db.collection("users").doc(user.uid).update({
+                            name: userName,
+                            neighbourhood: userNeighbourhood,
+                            city: userCity,
+                            phone: userPhone,
+                            profilePic: url // Save the URL into users collection
+                        })
+                            .then(function () {
+                                console.log('Added Profile Pic URL to Firestore.');
+                                console.log('Saved use profile info');
+                                document.getElementById('personalInfoFields').disabled = true;
+                            })
+                    })
+            })
+    })
+}
 
 var ImageFile;      //global variable to store the File Object reference
 
@@ -123,6 +123,9 @@ function chooseFileListener() {
 }
 chooseFileListener();
 
+const logoutButton = document.getElementById('logout-button');
+logoutButton.addEventListener('click', logout);
+
 //logout function
 function logout() {
     firebase.auth().signOut().then(() => {
@@ -132,10 +135,6 @@ function logout() {
         // An error happened.
     });
 }
-
-const logoutButton = document.getElementById('logout-button');
-logoutButton.addEventListener('click', logout);
-
 
 
 function getItems() {
