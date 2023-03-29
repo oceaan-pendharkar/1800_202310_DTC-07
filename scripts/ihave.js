9d2bcb4b0524bf3300ba5e1be6c0e17c097f2fab
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-03-29 11:17:28 -0700

    Fix checkboxes checked issue

--- scripts/ihave.js
+++ scripts/ihave.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
 });
 
 
-
 $(".close-btn").click(function () {
   $("#edit-items").modal('hide');
 });
@@ -18,7 +17,7 @@ $(".close-btn").click(function () {
 function generateCheckboxes() {
   const resourcesRef = firebase.firestore().collection('resources');
   const checkboxesContainer = document.getElementById('checkboxes');
-  
+
   resourcesRef.get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
       const docData = doc.data();
@@ -31,27 +30,50 @@ function generateCheckboxes() {
           const checkbox = document.createElement('input');
           checkbox.type = 'checkbox';
           checkbox.name = key;
+          checkbox.id = key;
           checkbox.value = docData[key];
-          
+
           // Create a label element
           const label = document.createElement('label');
-          label.textContent = key 
-          
+          label.textContent = key
+
           // Create a div element to wrap the checkbox and label
           const div = document.createElement('div');
           div.style.display = 'block';
           div.appendChild(checkbox);
           div.appendChild(label);
-          
+
+
           // Add the div element to the checkboxes container
           checkboxesContainer.appendChild(div);
 
+
+          firebase.auth().onAuthStateChanged(user => {
+            // Check if user is signed in:
+            if (user) {
+
+              //go to the correct user document by referencing to the user uid
+              currentUser = db.collection("users").doc(user.uid)
+              currentUser.get().then(userDoc => {
+                //get the user name
+                var items = userDoc.data().items;
+                if (items != null) {
+                  for (var i = 0; i < items.length; i++) {
+                    var item = items[i];
+                    var checkbox = document.getElementById(item);
+                    checkbox.checked = true;
+                  }
+                }
+              })
+            }
+          })
 
         }
       });
     });
   });
 }
+
 
 // Call the generateCheckboxes function
 generateCheckboxes();
@@ -62,47 +84,39 @@ generateCheckboxes();
 function saveResourceUpdate() {
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
   const IHaveList = [];
-  
+
   checkboxes.forEach((checkbox) => {
-      if (checkbox.checked) {
-        IHaveList.push(checkbox.value);
-      }
+    if (checkbox.checked) {
+      IHaveList.push(checkbox.value);
+    }
   });
-  firebase.auth().onAuthStateChanged( user => {
-              // Check if user is signed in:
-              if ( user ) {
-      
-                  //go to the correct user document by referencing to the user uid
-                  currentUser = db.collection( "users" ).doc( user.uid )
-                  return currentUser.update({
-                      items: IHaveList
-                  })
-                  .then(() => {
-                      console.log("Resource update saved successfully!");
-                      location.replace("thanksresourcesupdate.html")
-                  })
-                  .catch((error) => {
-                      console.error("Error saving resources: ", error);
-                  });
-      
-              } else {
-                  // No user is signed in.
-                  console.log( "No user is signed in" );
-              }
-          } );
+  firebase.auth().onAuthStateChanged(user => {
+    // Check if user is signed in:
+    if (user) {
+
+      //go to the correct user document by referencing to the user uid
+      currentUser = db.collection("users").doc(user.uid)
+      return currentUser.update({
+        items: IHaveList
+      })
+        .then(() => {
+          console.log("Resource update saved successfully!");
+          location.replace("thanksresourcesupdate.html")
+        })
+        .catch((error) => {
+          console.error("Error saving resources: ", error);
+        });
+
+    } else {
+      // No user is signed in.
+      console.log("No user is signed in");
+    }
+  });
 
 
   /// allow user to input their unique item
-  
-}
 
-currentUser.get().then(userDoc => {
-  //get the user name
-  var items = userDoc.data().items;
-  if (items.includes(docID)) {
-     document.getElementById('save-' + docID).innerText = 'checked';
-  }
-})
+}
 
 
 
@@ -110,31 +124,31 @@ function saveResourceInput() {
   newItem = document.getElementById('itemName').value;
 
 
-  
-  firebase.auth().onAuthStateChanged( user => {
-              // Check if user is signed in:
-              if ( user ) {
-      
-                  //go to the correct user document by referencing to the user uid
-                  currentUser = db.collection( "users" ).doc( user.uid )
-                  return currentUser.update({
-                      items: firebase.firestore.FieldValue.arrayUnion(newItem)
-                  }, { merge: true })
-                  .then(() => {
-                      console.log("Resource update saved successfully!");
-                      location.replace("thanksresourcesupdate.html")
-                  })
-                  .catch((error) => {
-                      console.error("Error saving resources: ", error);
-                  });
-      
-              } else {
-                  // No user is signed in.
-                  console.log( "No user is signed in" );
-              }
-          } );
-  
-  
+
+  firebase.auth().onAuthStateChanged(user => {
+    // Check if user is signed in:
+    if (user) {
+
+      //go to the correct user document by referencing to the user uid
+      currentUser = db.collection("users").doc(user.uid)
+      return currentUser.update({
+        items: firebase.firestore.FieldValue.arrayUnion(newItem)
+      }, { merge: true })
+        .then(() => {
+          console.log("Resource update saved successfully!");
+          location.replace("thanksresourcesupdate.html")
+        })
+        .catch((error) => {
+          console.error("Error saving resources: ", error);
+        });
+
+    } else {
+      // No user is signed in.
+      console.log("No user is signed in");
+    }
+  });
+
+
 
 
 }
