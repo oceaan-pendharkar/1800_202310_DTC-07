d9a285ee07d6313cfb8360bac6730df2e083fb3a
I-wen Su <grace2268@hotmail.com>
2023-04-01 01:13:13 -0700

    style and logo changed

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,14 @@ function getNameFromAuth() {
       // Do something for the currently logged-in user here: 
       console.log(user.uid); //print the uid in the browser console
       console.log(user.displayName);  //print the user name in the browser console
-      user_Name = user.displayName;
+      userName = user.displayName;
 
       //method #1:  insert with JS
       //document.getElementById("name-goes-here").innerText = userName;    
       // method #2:  insert using jquery
-      $("#name-goes-here").text(userName); //using jquery
+      // $("#name-goes-here").text(userName); //using jquery
       //method #3:  insert using querySelector
-      //document.querySelector("#name-goes-here").innerText = userName
+      document.querySelector("#name-goes-here").innerText = userName
 
     } else {
       // No user is signed in.
